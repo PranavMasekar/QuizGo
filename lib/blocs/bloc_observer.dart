7622c9115bb9f45d322f12815e51c1652ae77fd3
Pranav Masekar <pranavmasekar4@gmail.com>
2023-09-17 12:08:09 +0530

    Added BLoc Observer

---
+++ lib/blocs/bloc_observer.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log('${bloc.runtimeType} $event');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log('${bloc.runtimeType} $transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
+}
--- lib/blocs/export_bloc.dart
+++ lib/blocs/export_bloc.dart
@@ -1,4 +1,5 @@
 export './auth bloc/auth_bloc.dart';
+export './bloc_observer.dart';
 export './password cubit/password_cubit.dart';
 export './question bloc/question_bloc.dart';
 export './quiz bloc/quiz_bloc.dart';
