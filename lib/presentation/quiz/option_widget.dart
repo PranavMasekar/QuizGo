f22ddd13f340e7b56f8372268cbb95f644c8e222
PranavMasekar <pranavmasekar4@gmail.com>
2023-11-11 20:17:44 +0530

    Tested on IOS Simulator

--- lib/presentation/quiz/option_widget.dart
+++ lib/presentation/quiz/option_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -29,6 +31,7 @@ class OptionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log(unescape.convert(option.text));
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
       margin: EdgeInsets.only(bottom: 15.h),
